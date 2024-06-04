8ce9bda10e0cdbbe88c1f23e05cc3b8ca21b1273
Fiona Shan <yefengshan2024@u.northwestern.edu>
2024-06-04 15:05:44 +0000

    v3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,23 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by({ "email" => params["email"] })
+    if user
+      if BCrypt::Password.new(user["password"]) == params["password"]
+        session["user_id"] = user["id"]
+        redirect_to "/places"
+      else
+        flash["notice"] = "Unsuccessful login."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Unsuccessful login."
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    redirect_to "/login"
   end
 end
-  
